6929abfe584073f782f7d343c8631ec4bfc9339a
pratikalladi <71357691+pratikalladi@users.noreply.github.com>
2022-12-06 16:29:07 -0600

    Delete final_proj/build directory

--- final_proj/build/test_tests-b12d07c.cmake
+++
@@ -1,3 +0,0 @@
-add_test( sumDigits /workspaces/CS225/cs225-final-project/final_proj/build/test sumDigits  )
-set_tests_properties( sumDigits PROPERTIES WORKING_DIRECTORY /workspaces/CS225/cs225-final-project/final_proj/build)
-set( test_TESTS sumDigits)
