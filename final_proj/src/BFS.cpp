c157bf6120ed8a308f74814c6b1496a219e51b9b
wazo10 <dengwilliam@gmail.com>
2022-12-07 06:28:28 +0000

    implemented BFS with 1 compilation error

--- final_proj/src/BFS.cpp
+++ final_proj/src/BFS.cpp
@@ -3,3 +3,46 @@
  * For BFS
  */
 #include "BFS.h"
+#include "graph.h"
+std::vector<std::string> BFS(Graph::Node* source, Graph::Node* destination) {
+    std::vector<std::string> result;
+    std::vector<bool> visited(14111, false);
+    std::queue<Graph::Node*> bfs;
+    bfs.push(source);
+    Graph::Node* current = source;
+    visited.at(current->index) = true;
+    while (!bfs.empty()) {
+        current = bfs.front();
+        result.push_back(current->name);
+        for (auto iter : getNodeNeighbors(current->name)) {//help fix this error pls
+            if (visited.at(iter->index) == false) {
+                bfs.push(iter);
+                visited.at(iter->index) = true;
+            }
+        }
+        bfs.pop();
+    }
+    return result;
+}
+/*
+std::vector<std::string> BFS(int origin_airport) {
+    std::vector<std::string> result;
+    std::vector<bool> visited(14111, false);
+    std::queue<int> bfs;
+    bfs.push(origin_airport);
+    int current_airport = origin_airport;
+    visited.at(current_airport) true;
+    while (!bfs.empty()) {
+        current_airport = bfs.front();
+        result.push_back(name);
+        for (auto iter : graph of all flights that originate at current_airport) {
+            if (visited.at(iter.first) == false) {
+                bfs.push(iter.first);
+                visited.at(iter.first) = true;
+            }
+        }
+        bfs.pop();
+    }
+    return result;
+}
+*/
--- final_proj/src/BFS.h
+++ final_proj/src/BFS.h
@@ -2,5 +2,8 @@
  * @file BFS.h
  * 
  */
-
 #pragma once
+#include <vector>
+#include <queue>
+#include "graph.h"
+std::vector<std::string> BFS(Graph::Node* source, Graph::Node* destination);
