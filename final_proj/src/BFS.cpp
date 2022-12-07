5263e5124224b14c711c6b225d34815aa844742d
wazo10 <dengwilliam@gmail.com>
2022-12-07 06:28:46 +0000

    implemented BFS with 1 compilation error

--- final_proj/src/BFS.cpp
+++ final_proj/src/BFS.cpp
@@ -24,25 +24,3 @@ std::vector<std::string> BFS(Graph::Node* source, Graph::Node* destination) {
     }
     return result;
 }
-/*
-std::vector<std::string> BFS(int origin_airport) {
-    std::vector<std::string> result;
-    std::vector<bool> visited(14111, false);
-    std::queue<int> bfs;
-    bfs.push(origin_airport);
-    int current_airport = origin_airport;
-    visited.at(current_airport) true;
-    while (!bfs.empty()) {
-        current_airport = bfs.front();
-        result.push_back(name);
-        for (auto iter : graph of all flights that originate at current_airport) {
-            if (visited.at(iter.first) == false) {
-                bfs.push(iter.first);
-                visited.at(iter.first) = true;
-            }
-        }
-        bfs.pop();
-    }
-    return result;
-}
-*/
